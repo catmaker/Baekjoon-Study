51ca69bf1343cf73dd3947321d8888a484105fda
catmaker <rzt0317@nate.com>
2024-08-20 16:52:41 +0900

    2751 문제 풀이

---
+++ Level/Silver/5/2751/2751.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+// 파일 경로 설정
+const filePath = path.join(__dirname, "2751.txt");
+const file = process.platform === "linux" ? "/dev/stdin" : filePath;
+
+// 입력 데이터를 불러와서 처리
+let input = fs
+  .readFileSync(file)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((line) => line.trim())
+  .map(Number);
+
+let set = [...new Set(input.slice(1, input.length))];
+console.log(set);
+let sort = set.sort((a, b) => a - b);
+console.log(...sort);
