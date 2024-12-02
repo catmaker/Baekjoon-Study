f10fc7b7fa1ceaf73732b685d175aad2cc6877ff
신지수 <114233139+catmaker@users.noreply.github.com>
2024-12-02 15:43:55 +0900

    변수 선언과 자료형

---
+++ C++/변수 선언과 자료형.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h> // 입출력 라이브러리 포함 
+
+using namespace std; // 표준 네임스페이스 사용 
+int main(){
+	
+	int a = 10;
+	float b = 3.14f; // f는 float 형임을 나타낸다. 
+	double c = 3.14159;
+	char d = 'A';
+	bool e = true;
+	
+	cout << "변수들 모두 출력하는데 라인이 변경되어야 한다." << endl << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+	cout << e << endl;
+	
+	cout << "변수들 모두 출력하는데 라인이 변경되어야 한다." << endl << a << endl << b << endl << c << endl << d << endl << e << endl;
+	
+	cout << "정수형 변수 a: " << a << endl; 
+	cout << "단정도 실수형  변수 b: " << b << endl;
+	cout << "배정도 실수형 변수 c: " << c << endl;
+	cout << "문자형 변수 d: " << d << endl;
+	cout << "불리언형 변수 e: " << e << endl;
+}
