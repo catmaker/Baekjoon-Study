9890f68724e5c49ebeec1ff52c98d1d1ec9602fc
신지수 <114233139+catmaker@users.noreply.github.com>
2024-12-02 16:11:19 +0900

    Create while문.cpp

---
+++ C++/while문.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int i = 1;
+	while (i <= 5 ) {
+		cout << "현재 값은: " << i << endl;
+		i++;
+		if (i > 3) {
+			return 0;
+		}
+	}
+	return 0;
+}
