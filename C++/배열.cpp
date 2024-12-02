fbfc561f1c9b8b1804e1592563bc9fc59d1c3633
신지수 <114233139+catmaker@users.noreply.github.com>
2024-12-02 16:13:22 +0900

    Create 배열.cpp

---
+++ C++/배열.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int arr[5] = {10,20,30,40,50};
+	
+	for(int i = 0; i < 5; i++) {
+		cout << "배열의 " << i + 1 << "번째 값은: " << arr[i] << endl;
+	}
+	return 0;
+}
