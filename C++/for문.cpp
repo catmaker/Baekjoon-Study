2bba0a877feaa58540fa5812f7b95964147bce4e
신지수 <114233139+catmaker@users.noreply.github.com>
2024-12-02 16:09:10 +0900

    for문

---
+++ C++/for문.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main () {
+	for (int i = 1; i <=5; i++) {
+		if (i > 3) {
+			return 0;
+		}
+		cout << "현재 값은: " << i << endl; 
+	}
+	return 0;
+}
