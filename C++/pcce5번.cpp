ea282f0c9d4235efdd21840730cf9c247fb952ba
신지수 <114233139+catmaker@users.noreply.github.com>
2024-12-03 11:27:18 +0900

    Create pcce5번.cpp

---
+++ C++/pcce5번.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<int> numbers, vector<int> our_score, vector<int> score_list) {
+    int num_student = numbers.size();
+    vector<string> answer(num_student);
+    
+    for (int i = 0; i < num_student; i++) {
+        if (our_score[i] == score_list[numbers[i]-1]) {
+            answer[i] = "Same";
+        }
+        else {
+            answer[i] = "Different";
+        }
+    }
+    
+    return answer;
+}
+
+
+// 배열의 인덱스를 주의해야한다. 0부터 시작
